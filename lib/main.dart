800ca96e5ec807ff09f3cde34eea391bf0c92c35
Miguel Angel <mgangsal@outlook.com>
2023-04-22 18:38:02 -0500

    Using lists inside of lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker_app/expenses.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expenses.dart';
 
 void main() {
   runApp(const MaterialApp(home: Expenses()));
